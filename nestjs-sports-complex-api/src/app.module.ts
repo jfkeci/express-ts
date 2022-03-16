d97c6ce0c1b9dd6f11d9b162b4c080ee5871fe77
jfkeci <ja.lip132@gmail.com>
2022-03-16 09:21:10 +0100

    chore: let app use .env with @nestjs/config ConfigModule

--- nestjs-sports-complex-api/src/app.module.ts
+++ nestjs-sports-complex-api/src/app.module.ts
@@ -2,20 +2,22 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { EnrollmentsModule } from './enrollments/enrollments.module';
 import { SportsModule } from './sports/sports.module';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env.DB_USER)
 
 @Module({
   imports: [
     UsersModule,
-    ProductsModule,
+    SportsModule,
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gg0cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     ),
     EnrollmentsModule,
-    SportsModule
   ],
   controllers: [AppController],
   providers: [AppService],
