a84435dfc72aae8c60a666f7cbea51c26ac19c5e
jfkeci <ja.lip132@gmail.com>
2022-03-16 07:16:02 +0100

    chore: init nestjs-mongodb project

---
+++ nestjs-mongodb/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://bebabit:bebabit@cluster0.gg0cz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
