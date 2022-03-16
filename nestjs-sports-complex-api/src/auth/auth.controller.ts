12f09ef72371ee68ec9081487a9f8519848b4811
jfkeci <ja.lip132@gmail.com>
2022-03-16 12:58:31 +0100

    feat: request body validation

--- nestjs-sports-complex-api/src/auth/auth.controller.ts
+++ nestjs-sports-complex-api/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { NewUserDTO } from 'src/user/dto/user.dto';
 import { sendEmail } from 'src/utils/mailer';
 import { AuthService } from './auth.service';
@@ -8,11 +8,13 @@ export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('user')
+    @UsePipes(new ValidationPipe())
     async createUser(@Body() user: NewUserDTO) {
         return await this.authService.register(user, 'user')
     }
 
     @Post('admin')
+    @UsePipes(new ValidationPipe())
     async createAdmin(@Body() user: NewUserDTO) {
         return await this.authService.register(user, 'admin')
     }
--- nestjs-sports-complex-api/src/user/dto/user.dto.ts
+++ nestjs-sports-complex-api/src/user/dto/user.dto.ts
@@ -1,10 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class NewUserDTO {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
 
 export class ExistingUserDTO {
+    @IsString()
+    @IsNotEmpty()
     email: string;
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
