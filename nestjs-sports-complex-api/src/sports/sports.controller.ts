f53bd303a337e149c47835dd3c778547f70f305a
jfkeci <ja.lip132@gmail.com>
2022-03-17 10:51:51 +0100

    chore: add status codes to requests in sports controller

--- nestjs-sports-complex-api/src/sports/sports.controller.ts
+++ nestjs-sports-complex-api/src/sports/sports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, HttpCode } from '@nestjs/common';
 import { SportsService } from './sports.service';
 import { SportDTO } from './dto/sport.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,30 +9,35 @@ export class SportsController {
   constructor(private readonly sportsService: SportsService) { }
 
   @Post()
+  @HttpCode(201)
   @UseGuards(AdminAuthGuard)
   create(@Body() dto: SportDTO) {
     return this.sportsService.create(dto);
   }
 
   @Get()
+  @HttpCode(200)
   @UseGuards(PublicAuthGuard)
   findAll() {
     return this.sportsService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(200)
   @UseGuards(PublicAuthGuard)
   findOne(@Param('id') id: string) {
     return this.sportsService.findOne(id);
   }
 
   @Patch(':id')
+  @HttpCode(204)
   @UseGuards(AdminAuthGuard)
   update(@Param('id') id: string, @Body() dto: SportDTO) {
     return this.sportsService.update(id, dto);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   @UseGuards(AdminAuthGuard)
   remove(@Param('id') id: string) {
     return this.sportsService.remove(id);
