e2742ca12c0099a371190910fba025cf3f34762a
jfkeci <ja.lip132@gmail.com>
2022-03-17 10:50:56 +0100

    chore: add rating model

---
+++ nestjs-sports-complex-api/src/ratings/rating.model.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+export const RatingSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            maxLength: 255,
+            required: true
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+            required: true
+        },
+        userId: {
+            type: String,
+            required: true
+        },
+        classId: {
+            type: String,
+            required: true
+        }
+    }, { timestamps: true }
+);
+
+export interface Rating extends Document {
+    text: string;
+    rating: number;
+    userId: string;
+    classId: string;
+}
