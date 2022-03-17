11a7316e5ad88058f51006e0623a2be94ec0294a
jfkeci <ja.lip132@gmail.com>
2022-03-17 09:52:14 +0100

    chore: make sport model

---
+++ nestjs-sports-complex-api/src/sports/sport.model.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+export const SportSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+    }, { timestamps: true }
+);
+
+export interface Sport extends Document {
+    name: string;
+}
